f93dc2ceab5138fe01c4ab3705057c0fa0c832a4
fujitenmon-coder <fuji.tenmon@gmail.com>
2026-04-27 16:56:36 +0900

    excelへの出力を関数化、ERRORへの対応を追加

--- seeing_allframe.py
+++ seeing_allframe.py
@@ -19,31 +19,40 @@ import datetime
 import time
 from pathlib import Path
 import glob
+import time
 from tkinter import filedialog as fd
 from scipy.interpolate import CubicSpline
 from scipy.optimize import minimize_scalar
-#実行とデバックにて走らせてください
+from win11toast import toast
+import sys
+#20260206
+argv=sys.argv
+a1 = sys.argv[1]
+a2=int(sys.argv[2])
 
-#20260106
-patt=r"J:\2025-08-30Z\2025-08-30-LT\2025-08-30-0448_4-CapObj"
-Vdir =patt.split(":")[0]+":"
-V_folderName=patt.split(":")[1]
-dir0= "J:\\"
-dir=dir0+'img\\'
-dirr = dir0 + "result\\"
-sdir1=dir0+'simg\\'
-sdir2=dir0+'sprof\\'
-sdir3=dir0+"3d\\"
-scsv=dir0+"position.csv"
-#V_dirs  = glob.glob(Vdir+V_folderName +"\\"+ "*\\")
-V_dirs  = glob.glob(Vdir+V_folderName + "*\\")
-results = []
-sizes = []
+#実行とデバックにて走らせてください
+pats=a1
+ind=a2
+outdir=r"C:\Users\fujit\OneDrive\デスクトップ\allfram"
+#outdir=r"C:\Users\fujit\OneDrive\デスクトップ\allfram"
+
 gap = 170
-JUNSUIGaps=[]
 limb_wigth = 48 #単位はpix
 limb_wigth = int(limb_wigth / 2)
 pad = 5
+
+houghC_param1=30
+houghC_param2=100
+houghTF=True
+
+
+start=time.time()
+def folder_read(fpath):
+    folders = [
+        name for name in os.listdir(fpath)
+        if os.path.isdir(os.path.join(fpath, name))
+        ]
+    return folders
 def sengiri(file):
     lst = np.empty(gap*8)
     #plt.clf()
@@ -54,15 +63,16 @@ def sengiri(file):
     img=cv2.imread(file,0)
     #-- 太陽像を円と考えて検出する --
     #（※太陽像の真ん中がサチレーションしているので、うまく検出できていない：宿題）
-    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp=3,minDist=80,param1=30,param2=100, minRadius=0, maxRadius=0)
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp=3,minDist=80,param1=houghC_param1,param2=houghC_param2, minRadius=0, maxRadius=0)
     if circles is None:
+        houghTF=False
         return None
     circles = np.array(circles)
     #-- 検出された円の中心(xc,yc)と円の半径r --
     xc=np.int64(circles[0,0,0])
     yc=np.int64(circles[0,0,1])
     r=np.int64(circles[0,0,2])
-    print(r)
+    #print(r)
     if file.endswith(".tiff" or ".tif"):
         img=cv2.imread(file,-1)
 
@@ -101,20 +111,6 @@ def sengiri(file):
         real_r = gaps_r - limb_wigth + index_Max_d_sample#limbまでの距離 index_Max_d_sampleをx2.xに変えると補完*4
         lst[gaps+gap] = real_r - gaps_r
         
-        """
-        print(y_max,position[0]/pix_dis)
-        plt.plot(x, sample, 'o', label='Original Data')
-        plt.plot(x_dense, cs(x_dense), label='Cubic Spline')
-        plt.plot(x_dense, cs_g(x_dense), label='Cubic Spline_gradiented')
-        plt.plot(max_x.x, cs(max_x.x), 'rx', label='Maximum')
-
-        #plt.plot(x_dense, poly_lagrange(x_dense), label='Lagrange Polynomial')
-        #plt.plot(x_dense, poly_fit(x_dense), label='2nd Degree Fit')
-        plt.legend()
-        plt.grid()
-        plt.show()
-        exit()
-        """     
         #plt.plot([xc-gaps_r-limb_wigth,xc-gaps_r+limb_wigth],[yc+gaps,yc+gaps],color="b")
 
         #繰り返し
@@ -170,11 +166,10 @@ def sengiri(file):
         lst[gaps+(gap*7)] = real_r - gaps_r
 
     return lst
-
 def Kansu(Sam_dir,executor=None):
     foldername = os.path.basename(os.path.dirname(Sam_dir))
     #print()
-    #print(foldername)
+    #print("fname",foldername)
     if "LT" in foldername :
         location = "LoofTop"
     elif "PL" in foldername :
@@ -185,9 +180,8 @@ def Kansu(Sam_dir,executor=None):
     #lstはシーイングサイズを格納
     lst = []
     #sizeは太陽のサイズ（半径）を格納するリスト
-    sizes = []
-    files = glob.glob(Sam_dir+"*.tiff") + glob.glob(Sam_dir+"*.tif") + glob.glob(Sam_dir+"*.jpg")
-
+    files = glob.glob(Sam_dir+"\\*.tiff") + glob.glob(Sam_dir+"\\*.tif") + glob.glob(Sam_dir+"\\*.jpg")
+    #print("filesssss",files)
     with tqdm.tqdm(total=len(files)) as progress:
         lsts=ex.map(sengiri, files)
         for data in lsts:
@@ -209,94 +203,77 @@ def Kansu(Sam_dir,executor=None):
         sts.append(float(st))
         if st_max < st:
             st_max = st
-            mxcount= counter
         if st_min > st:
             st_min = st
-            mncount=counter
         counter += 1
-    print(sts)
-    Sts=sts
+    
     #print("MAX",st_max,"frame",mxcount)
     #print("MIN",st_min,"frame",mncount)
     print("median",np.median(sts))
     print("average",np.average(sts))
     #print("size",np.average(sizes))
     #print("location",location)
-    """
-    plt.xlim(0,1000)
-    plt.ylim(0, 9)
-    plt.grid(True)
-
-    plt.scatter(range(len(sts)),sts,color = "#003CFF",s=5)
-    plt.savefig(dirr + foldername + "sanpuNew.png")
-    """
-    x = []
-    for i in range(len(sts)):
-        x.append(i)
+    return sts
+def llen(ist):
+    return range(len((ist)))
+def make_sheet(result,outpath,mode,nc):
+    colmsnumber=0
+    colms=[]
+    data=[]
     
-    #ここは移動平均
-    """
-    n_conv = 10
-    b = np.ones(n_conv)/n_conv 
-    sts1 = np.convolve(sts, b , mode="same")
-    sts1mx=0
-    sts1mn=9999999
-    sts1counter = 0  
-    sts1mxcount = 0
-    sts1mncount = 0
-    for i in range(10,len(sts1)-10):
-        if sts1mx < i:
-            sts1mx = i
-            sts1mxcount= counter
-        if sts1mn > i:
-            sts1mn = i
-            sts1mncount=counter
-        counter += 1
-    fig=plt.figure(figsize=(15,5))
-    ax=fig.add_subplot(111)
-    ax.plot(x[100:130],sts[100:130],color="#E05BB4B0",lw = 10)
-    sts1[0:9]=0
-    sts1[len(sts1)-10:len(sts1)]=0
-    ax.plot(x[100:130],sts1[100:130],"--",color="#BB039C",lw = 7)
-    ax.set_title(foldername)
-    #ax.set_xlabel("frame")
-    #ax.set_ylabel("standard deviation")"
-    if not os.path.isdir(dirr+"averages"+"\\"):
-        os.makedirs(dirr+"averages"+"\\")
-    plt.savefig(dirr+"averages"+"\\"+foldername+".png")
-    #ここまでが移動平均
-    """
-    lst = np.array([V_folderName + foldername,st_max,mxcount,st_min,mncount,datetime.datetime.now().replace(microsecond=0),np.median(sts),np.average(sts),location])#sts1mx,sts1mn])
-    lst = lst.reshape(len(lst),1)
-    #この行でエラーが出る場合はプログラムフォルダ内に「result」フォルダを作成し、「results.xlsx」を作成してください。
-    df = pd.DataFrame(lst.T, columns=["name", "max", "max_frame", "min", "min_frame","Date time","median","average","location"])#,"移動平均mx","移動平均mn"])
-    return df,Sts
-ja=0
+    if mode=="a": 
+        df = pd.read_excel(outpath)
+        colms = df.columns.tolist()   # 列名のリスト
+        data = df.values.tolist()       #データのリスト
+    else:
+        colms = []
+        data=[]
+    colmsnumber=len(colms)
+    colms.append(nc)
+    if len(data)>len(result):
+        for i in range(len(data)-len(result)):
+            result.append(None)
+    for re in llen(result):
+        if len(data)<=re:
+            data.append([])
+        for i in range(colmsnumber-len(data[re])):
+            data[re].append(None)
+        data[re].append(result[re])
+        
+    return data,colms   
+if __name__=="__main__":#関数の呼び出し時は起動しない
+    foldist=folder_read(pats)
+    #print("foldist",foldist)
+    if foldist==[]:
+        foldist=[pats]
+    #もしpats内にディレクトリがなければpats内の画像を処理するものと解釈
+    for i in foldist:
+        print("I DO",foldist.index(i),"/in",len(foldist))
+        try:
+            if pats != i:#pats内にディレクトリがない場合に困るから条件分岐
+                patt=pats+"\\"+(i)
+                out_path=outdir+"\\"+pats.split("\\")[ind]+"_by_"+__file__.split("\\")[-1]+".xlsx"#出力xlsxのパスを作成
+            else:
+                patt=pats
+                out_path=outdir+"\\"+pats.split("\\")[-1]+"_by_"+__file__.split("\\")[-1]+".xlsx"#出力xlsxのパスを作成
+            with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as ex:
+                #↑並列処理用のやつ
+                result=Kansu(patt,ex)#主処理、フォルダのパスと並列処理のやつが引数。
 
-if __name__ == '__main__':
-    Df=pd.DataFrame()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as ex:
-        for i in V_dirs:
-            print(os.path.basename(os.path.dirname(i)))
-            add = Kansu(i,ex)[0]
-            Df = pd.concat([Df,add])
-            #print()
-            #plt.close
-            #Df.to_csv(dirr+V_folderName+"result"+".csv", index=False,)
-            Df.to_csv("C:\\Users\\fujit\\OneDrive\\デスクトップ\\cop_gaps_seev2.csv", index=False,)
-            JUNSUIGaps+=Kansu(i,ex)[1]
             
-        ja=1
+            mode="a" if os.path.exists(out_path) else "w"#出力xlsxが既存か否かによってmake_sheetのmodeを変更
+            data,colms=make_sheet(result,out_path,mode,nc=i)#xlsxのデータを作成
+            df = pd.DataFrame(data, columns=colms)#書き込み用のやつ
+            df.to_excel(out_path, index=False)#書き込み
 
-
-import csv
-print('JUNSUI',JUNSUIGaps,'JUNSI')
-name=V_folderName.split("\\")
-name=name[-2]+"_"+name[-1]+".csv"
-if ja==1:
-    with open("C:\\Users\\fujit\\OneDrive\\デスクトップ\\allframeseeiingbyhahu\\"+name,mode='w') as fe:
-            file=csv.writer(fe)
-            file.writerow(JUNSUIGaps)
-    print(f"CSVファイル  にデータを書き出しました。")
-    
-print(patt)
+            toast("Python通知", f"{i}が終了しました\nfrom {__file__.split("\\")[-1]}")
+            print(patt)
+            print(out_path)
+        except:
+            if not houghTF :
+                error_reason="HOUGH"
+            else:
+                error_reason="UNKNOWN"
+            toast("Python通知",f"ERROR\nwhy error{error_reason}\nfrom {__file__.split("\\")[-1]}")
+    toast("Python通知",f"{pats}の処理がすべて終了しました。\nfrom {__file__.split("\\")[-1]}")
+    print("time:",start-time.time())
