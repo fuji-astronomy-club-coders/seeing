fe04a7d834c6dce2f9ecaa8c62d672d99014b560
fujitenmon-coder <fuji.tenmon@gmail.com>
2026-05-07 17:49:04 +0900

    2回微分と1回微分の比較
    1回微分と2回微分の縁の点の座標を返す、
    sengiri_X2_justOUTside_edgepointsを作成
    (indexから座標に変換する過程でmiss)
    それを描画するone~~~DROW.pyを作成
    sampleのtiffを用意

---
+++ one_or_two_diffDROW.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from sengiries import sengiri_X2_justOUTside_edgepoints
+
+sun_image=r"J:\2025-08-30Z\2025-08-30-PL\2025-08-30-0441_9-CapObj\img00000001.tiff"
+
+two,one=sengiri_X2_justOUTside_edgepoints(sun_image)
+plt.imshow(cv2.imread(sun_image, cv2.IMREAD_UNCHANGED), cmap='gray')
+size=3.5
+plt.scatter(two[0], two[1], color='cyan', label='twice diff max', s=size)
+plt.scatter(one[0], one[1], color='magenta', label='first diff max', s=size)
+plt.legend()    
+plt.show()
+print ("data:", sun_image)
--- sengiries.py
+++ sengiries.py
@@ -196,7 +196,89 @@ def sengiri_X2_justOUTside(
                     print("Msapl", Maxd_sample)
             
     return lst
+def sengiri_X2_justOUTside_edgepoints(    
+    file
+    ,gap=170
+    ,limb_wigth = 24 
+    ):
+    """
+    listです！！説明してる暇はありません！！
+    """
+    twox,twoy=[],[]
+    onex,oney=[],[]
+    if file.endswith(".tiff" or ".tif"):
+        lst = np.zeros(gap*8)
+        xc, yc, r = MIN2(file)
+        img= cv2.imread(file,cv2.IMREAD_UNCHANGED)
+        for gaps in range(-gap , gap):
+        
+            #plt.clf()
+            #太陽の縁を探索
+            gaps_r =int(np.sqrt(np.abs(r**2 - gaps**2)))
+            gaps_r = int(gaps_r)
+            gaps = int(gaps)
+            #  memo img[top : bottom, left : right]
+            
+            pl=["L","R","T","B"]
+            xyof=[
+                (gaps,gaps+1)
+                ,[(-gaps_r-limb_wigth,-gaps_r+limb_wigth),(gaps_r-limb_wigth,gaps_r+limb_wigth)]
+            ]
+            #縦倒し,逆順
+            pldc={
+                "L":(True,0),
+                "R":(True,1),
+                "T":(False,0),
+                "B":(False,1)
+                }
+            for place in pl:
+                
+                pld = pldc[place]
+                "↑参照しておくよん"
+                if pld[0]:
+                    y_of,x_of = xyof[0],xyof[1][pld[1]] 
+                elif not pld[0]:
+                    y_of,x_of = xyof[1][pld[1]],xyof[0]
+                sample=img[int(yc+y_of[0]):int(yc+y_of[1]),int(xc+x_of[0]):int(xc+x_of[1])]
+                sample=np.array(np.ravel(sample))
+                if bool(pld[1]):
+                    sample=np.flip(sample)
+                fd_sample = np.abs(np.gradient(sample))
+                Maxd_sample = np.amax(fd_sample)
+                findex_Max_d_sample = np.where(fd_sample == Maxd_sample)[0][0]
+                d_sample = np.abs(np.gradient(np.gradient(sample)))
+                if pldc[place][1] == 1:#縦倒しのとき
+                    d_sample=d_sample[:findex_Max_d_sample]
+                else:
+                    d_sample=d_sample[findex_Max_d_sample:]
+                Maxd_sample=np.max(d_sample)
+                index_Max_d_sample = np.where(d_sample == Maxd_sample)[0][0]
+                index_Max_d_sample = 2*limb_wigth-index_Max_d_sample if bool(pld[1]) else index_Max_d_sample
+                real_r = gaps_r - limb_wigth +index_Max_d_sample +(-limb_wigth if not bool(pld[1]) else findex_Max_d_sample)#limbまでの距離
+                freal_r = gaps_r - limb_wigth +findex_Max_d_sample#limbまでの距離
+                if place == "L":
+                    twox.append(xc-real_r)
+                    twoy.append(yc+gaps)
+                    onex.append(xc+-freal_r)
+                    oney.append(yc+gaps)
+                elif place == "R":
+                    twox.append(xc+real_r)
+                    twoy.append(yc+gaps)
+                    onex.append(xc+freal_r)
+                    oney.append(yc+gaps)
+                elif place == "T":
+                    twox.append(xc+gaps)
+                    twoy.append(yc-real_r)
+                    onex.append(xc+gaps)
+                    oney.append(yc-freal_r)
+                elif place == "B":
+                    twox.append(xc+gaps)
+                    twoy.append(yc+real_r)
+                    onex.append(xc+gaps)
+                    oney.append(yc+freal_r)
+    return [twox,twoy],[onex,oney]
 
+    return twox,twoy
 def sengiri_X2_drowero(file,
                     lookgap=None,
                     gap=170,
